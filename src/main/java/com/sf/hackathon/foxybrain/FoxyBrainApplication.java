2966c85162dd9d22a17879b8973cb485308d166b
dragon0556 <avinashkumar@salesforce.com>
2019-10-08 17:32:57 +0530

    adding first set of code

---
+++ src/main/java/com/sf/hackathon/foxybrain/FoxyBrainApplication.java
@@ -0,0 +1,13 @@
+package com.sf.hackathon.foxybrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoxyBrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoxyBrainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sf/hackathon/foxybrain/FoxyBrainApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sf.hackathon.foxybrain;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FoxyBrainApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
